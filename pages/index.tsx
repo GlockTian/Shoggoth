8ff8a42ebbf09a51f3f02d55aaf3f39f304447ed
GlockTian <petertianzhong@gmail.com>
2022-11-10 15:13:25 +1300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,31 @@
-import Link from 'next/link'
-import dbConnect from '../util/dbConnect'
-import character from '../model/character'
-import Image from 'next/image'
+import Link from "next/link";
+import dbConnect from "../util/dbConnect";
+import Character from "../model/character";
+import Image from "next/image";
 interface Character {
-  _id: string
-  name: string
-  description: string
-  image_url: string
+  _id: string;
+  name: string;
+  description: string;
+  image_url: string;
 }
 
-
-const Index = ({ characters} : { characters: Character[] }) => 
-(  <>
+const Index = ({ characters }: { characters: Character[] }) => (
+  <>
     {characters.map((character) => (
       <div key={character._id}>
         <div className="card">
-          <img src={character.image_url} alt={character.name}/>
+          <img src={character.image_url} alt={character.name} />
           <h5 className="character-name">{character.name}</h5>
           <div className="main-content">
             <p className="character-name">{character.name}</p>
             <p className="description">Description: {character.description}</p>
 
             <div className="btn-container">
-              <Link href="/[id]/edit" as={`/${character._id}/edit`} legacyBehavior>
+              <Link
+                href="/[id]/edit"
+                as={`/${character._id}/edit`}
+                legacyBehavior
+              >
                 <button className="btn edit">Edit</button>
               </Link>
               <Link href="/[id]" as={`/${character._id}`} legacyBehavior>
@@ -33,21 +36,22 @@ const Index = ({ characters} : { characters: Character[] }) =>
         </div>
       </div>
     ))}
-  </>)
+  </>
+);
 
 /* Retrieves character(s) data from mongodb database */
-export async function getServerSideProps() {
-  await dbConnect()
+export async function getStaticProps() {
+  await dbConnect();
 
   /* find all the data in our database */
-  const result = await character.find({})
+  const result = await Character.find({});
   const characters = result.map((doc) => {
-    const character = doc.toObject()
-    character._id = character._id.toString()
-    return character
-  })
+    const character = doc.toObject();
+    character._id = character._id.toString();
+    return character;
+  });
 
-  return { props: { characters: characters } }
+  return { props: { characters: characters } };
 }
 
-export default Index
+export default Index;
